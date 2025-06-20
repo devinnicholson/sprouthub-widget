c98a86b059e8d78561e9e0283bd676bd0f8fc1ed
devinnicholson <devinnicholson@icloud.com>
2025-06-19 23:09:09 -0700

    build

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: 'public/index.html'
-    }
-  },
   server: {
     headers: {
       'X-Frame-Options': 'ALLOWALL',
       'Content-Security-Policy': "frame-ancestors *"
     }
   }
-})
+});
